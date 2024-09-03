e8efebf513f4a336b0d9e5bd73f8e52c830b1d70
jp9141joao <162065477+jp9141joao@users.noreply.github.com>
2024-09-03 19:59:24 +0000

    commit

--- planner-travel/src/App.tsx
+++ planner-travel/src/App.tsx
@@ -1,7 +1,7 @@
 import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import './App.css'
 import Home from "./pages/Home";
-import TravelHome from "./pages/TravelDetails";
+import TravelDetails from "./pages/TravelDetails";
 import EditTravel from "./pages/EditTravel";
 import AddTravel from "./pages/AddTravel";
 
@@ -13,7 +13,7 @@ function App() {
           <Route path="/" element={<Navigate to={'/home'}/>} />
           <Route path="/home" element={<Home/>}/>
           <Route path="/travel/add" element={<AddTravel/>}/>
-          <Route path="/travel/acess/:id" element={<TravelHome/>}/>
+          <Route path="/travel/details/:id" element={<TravelDetails/>}/>
           <Route path="/travel/edit/:id" element={<EditTravel/>}/>
         </Routes>
       </Router>
